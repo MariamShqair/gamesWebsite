539b7821209e2dbd738ca8bf39164be1c61f95eb
mariam <mariam@gmail.com>
2019-01-19 06:59:43 +0200

    Updates

---
+++ precache-manifest.4e00f87ee4c163831a73a1c38e64504e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "08a3fa5e8e0ac30826b1",
+    "url": "/static/css/main.e6172972.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "a8ea2470f1264ce13d66",
+    "url": "/static/js/1.a8ea2470.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "08a3fa5e8e0ac30826b1",
+    "url": "/static/js/main.08a3fa5e.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "a8ea2470f1264ce13d66",
+    "url": "/static/css/1.84bccec5.chunk.css"
+  },
+  {
+    "revision": "605ca0fc45808c778a6aeb6d2537da2f",
+    "url": "/index.html"
+  }
+];
