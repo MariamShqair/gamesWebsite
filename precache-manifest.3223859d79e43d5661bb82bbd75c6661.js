7a15740e81bf1cafc8c1ef6edf66f186ef8288a5
mariam <mariam@gmail.com>
2019-01-22 04:39:50 +0200

    Updates

--- precache-manifest.3223859d79e43d5661bb82bbd75c6661.js
+++ precache-manifest.3223859d79e43d5661bb82bbd75c6661.js
@@ -4,8 +4,8 @@ self.__precacheManifest = [
     "url": "/gamesWebsite/static/media/fontawesome-webfont.af7ae505.woff2"
   },
   {
-    "revision": "d41fcbf669678540a166",
-    "url": "/gamesWebsite/static/css/main.5a9c6d10.chunk.css"
+    "revision": "a1aa8d993b3ef5da952b",
+    "url": "/gamesWebsite/static/css/main.2d4da14c.chunk.css"
   },
   {
     "revision": "912ec66d7572ff821749319396470bde",
@@ -20,27 +20,27 @@ self.__precacheManifest = [
     "url": "/gamesWebsite/static/js/runtime~main.cab9fefe.js"
   },
   {
-    "revision": "d41fcbf669678540a166",
-    "url": "/gamesWebsite/static/js/main.d41fcbf6.chunk.js"
+    "revision": "a1aa8d993b3ef5da952b",
+    "url": "/gamesWebsite/static/js/main.a1aa8d99.chunk.js"
   },
   {
     "revision": "fee66e712a8a08eef5805a46892932ad",
     "url": "/gamesWebsite/static/media/fontawesome-webfont.fee66e71.woff"
   },
   {
-    "revision": "b06871f281fee6b241d60582ae9369b9",
-    "url": "/gamesWebsite/static/media/fontawesome-webfont.b06871f2.ttf"
-  },
-  {
     "revision": "674f50d287a8c48dc19ba404d20fe713",
     "url": "/gamesWebsite/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/gamesWebsite/static/media/fontawesome-webfont.b06871f2.ttf"
   },
   {
     "revision": "7a06b2892bfd0ad853f9",
     "url": "/gamesWebsite/static/css/1.d2154923.chunk.css"
   },
   {
-    "revision": "cca49d35e95a8c730be1a56f0d153498",
+    "revision": "20c70938460d507dadc87c177191d105",
     "url": "/gamesWebsite/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/gamesWebsite/precache-manifest.72c6551ca03a804cd358eed9d15b07a3.js"
+  "/gamesWebsite/precache-manifest.3223859d79e43d5661bb82bbd75c6661.js"
 );
 
 workbox.clientsClaim();
